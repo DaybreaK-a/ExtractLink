10a5189b9f58b5ffbce75dd3b2817d4f3a4a4f2f
DaybreaK-a <nik.bogdanov.05@mail.ru>
2024-09-08 23:17:41 +0300

    Add files via upload

---
+++ ExtractLInk/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace URLParser
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.txtInputFile = new System.Windows.Forms.TextBox();
+            this.btnInputFile = new System.Windows.Forms.Button();
+            this.txtOutputFilePath = new System.Windows.Forms.TextBox();
+            this.btnOutputFile = new System.Windows.Forms.Button();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.RichTextBox();
+            this.btnChangeTheme = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInputFile
+            // 
+            this.txtInputFile.Location = new System.Drawing.Point(12, 12);
+            this.txtInputFile.Name = "txtInputFile";
+            this.txtInputFile.Size = new System.Drawing.Size(379, 20);
+            this.txtInputFile.TabIndex = 0;
+            // 
+            // btnInputFile
+            // 
+            this.btnInputFile.Location = new System.Drawing.Point(433, 7);
+            this.btnInputFile.Name = "btnInputFile";
+            this.btnInputFile.Size = new System.Drawing.Size(100, 23);
+            this.btnInputFile.TabIndex = 1;
+            this.btnInputFile.Text = "Выбрать файл";
+            this.btnInputFile.UseVisualStyleBackColor = true;
+            this.btnInputFile.Click += new System.EventHandler(this.btnInputFile_Click);
+            // 
+            // txtOutputFilePath
+            // 
+            this.txtOutputFilePath.Location = new System.Drawing.Point(12, 38);
+            this.txtOutputFilePath.Name = "txtOutputFilePath";
+            this.txtOutputFilePath.Size = new System.Drawing.Size(379, 20);
+            this.txtOutputFilePath.TabIndex = 2;
+            // 
+            // btnOutputFile
+            // 
+            this.btnOutputFile.Location = new System.Drawing.Point(433, 33);
+            this.btnOutputFile.Name = "btnOutputFile";
+            this.btnOutputFile.Size = new System.Drawing.Size(100, 23);
+            this.btnOutputFile.TabIndex = 3;
+            this.btnOutputFile.Text = "Выбрать файл";
+            this.btnOutputFile.UseVisualStyleBackColor = true;
+            this.btnOutputFile.Click += new System.EventHandler(this.btnOutputFile_Click);
+            // 
+            // txtLink
+            // 
+            this.txtLink.Location = new System.Drawing.Point(12, 64);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(379, 20);
+            this.txtLink.TabIndex = 4;
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(433, 61);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(100, 23);
+            this.btnParse.TabIndex = 5;
+            this.btnParse.Text = "Ввести";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 90);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(521, 150);
+            this.txtResult.TabIndex = 6;
+            this.txtResult.Text = "";
+            // 
+            // btnChangeTheme
+            // 
+            this.btnChangeTheme.Location = new System.Drawing.Point(12, 246);
+            this.btnChangeTheme.Name = "btnChangeTheme";
+            this.btnChangeTheme.Size = new System.Drawing.Size(100, 23);
+            this.btnChangeTheme.TabIndex = 7;
+            this.btnChangeTheme.Text = "Сменить тему";
+            this.btnChangeTheme.UseVisualStyleBackColor = true;
+            this.btnChangeTheme.Click += new System.EventHandler(this.btnChangeTheme_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(433, 246);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(100, 23);
+            this.btnHelp.TabIndex = 8;
+            this.btnHelp.Text = "Справка";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(545, 281);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnChangeTheme);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnParse);
+            this.Controls.Add(this.txtLink);
+            this.Controls.Add(this.btnOutputFile);
+            this.Controls.Add(this.txtOutputFilePath);
+            this.Controls.Add(this.txtInputFile);
+            this.Controls.Add(this.btnInputFile);
+            this.Name = "Form1";
+            this.Text = "URL Parser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInputFile;
+        private System.Windows.Forms.Button btnInputFile;
+        private System.Windows.Forms.TextBox txtOutputFilePath;
+        private System.Windows.Forms.Button btnOutputFile;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.RichTextBox txtResult;
+        private System.Windows.Forms.Button btnChangeTheme;
+        private System.Windows.Forms.Button btnHelp;
+    }
+}
---
+++ ExtractLInk/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace URLParser
+{
+    public partial class Form1 : Form
+    {
+        // Словарь известных протоколов и портов
+        private readonly Dictionary<string, int> knownProtocols = new Dictionary<string, int>
+        {
+            { "http", 80 },
+            { "https", 443 },
+            { "ftp", 21 }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Кнопка для выбора исходного файла
+        private void btnInputFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtInputFile.Text = openFileDialog.FileName;
+            }
+        }
+
+        // Кнопка для выбора выходного файла
+        private void btnOutputFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtOutputFilePath.Text = saveFileDialog.FileName;
+            }
+        }
+
+        // Кнопка для парсинга ссылки или файла
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            // Очищаем вывод перед каждым разбором
+            txtResult.Clear();
+
+            if (!string.IsNullOrEmpty(txtLink.Text))
+            {
+                // Парсинг ссылки из текстового поля
+                ParseUrl(txtLink.Text);
+            }
+            else if (!string.IsNullOrEmpty(txtInputFile.Text) && File.Exists(txtInputFile.Text))
+            {
+                // Парсинг ссылок из файла
+                ParseFromFile(txtInputFile.Text, txtOutputFilePath.Text);
+            }
+            else
+            {
+                MessageBox.Show("Введите ссылку или выберите файл.");
+            }
+        }
+
+        // Метод для парсинга URL
+        private void ParseUrl(string url)
+        {
+            string result = ParseSingleUrl(url);
+
+            // Вывод результата
+            txtResult.Text = result;
+
+            // Если указан выходной файл, записать результат туда
+            if (!string.IsNullOrEmpty(txtOutputFilePath.Text))
+            {
+                File.AppendAllText(txtOutputFilePath.Text, result + Environment.NewLine);
+            }
+        }
+
+        // Парсинг ссылок из файла
+        private void ParseFromFile(string inputFile, string outputFile)
+        {
+            try
+            {
+                var lines = File.ReadAllLines(inputFile);
+                var results = new List<string>();
+
+                foreach (var line in lines)
+                {
+                    string result = ParseSingleUrl(line);
+                    results.Add(result);
+
+                    // Выводим результат в текстовое поле
+                    txtResult.AppendText(result + Environment.NewLine);
+                }
+
+                // Сохранение в файл, если указан выходной путь
+                if (!string.IsNullOrEmpty(outputFile))
+                {
+                    File.WriteAllLines(outputFile, results);
+                    MessageBox.Show("Результаты сохранены в файл.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        // Парсинг одной ссылки
+        private string ParseSingleUrl(string url)
+        {
+            try
+            {
+                Uri uri = new Uri(url);
+                string protocol = uri.Scheme;
+                string host = uri.Host;
+                int port = uri.Port != -1 ? uri.Port : (knownProtocols.ContainsKey(protocol) ? knownProtocols[protocol] : -1);
+
+                // Удаление начального слэша у URL-пути
+                string path = uri.AbsolutePath.TrimStart('/');
+
+                // Проверка, если путь пустой, считаем ссылку неправильной
+                if (string.IsNullOrEmpty(path))
+                {
+                    throw new UriFormatException();
+                }
+
+                // Если порт -1, значит ссылка неверная
+                if (port == -1)
+                {
+                    throw new UriFormatException();
+                }
+
+                // Форматированный вывод без жирного шрифта
+                return $"Протокол: {protocol}\nХост: {host}\nПорт: {port}\nURL-путь: {path}\n--------------------------------------";
+            }
+            catch (UriFormatException)
+            {
+                return "Ссылка не распознана";
+            }
+        }
+
+        // Смена цветовой темы
+        private void btnChangeTheme_Click(object sender, EventArgs e)
+        {
+            if (BackColor == SystemColors.Control)
+            {
+                BackColor = System.Drawing.Color.Black;
+                ForeColor = System.Drawing.Color.White;
+
+                // Темная тема для всех элементов
+                foreach (Control control in this.Controls)
+                {
+                    if (control is Button || control is TextBox || control is RichTextBox)
+                    {
+                        control.BackColor = System.Drawing.Color.Gray;
+                        control.ForeColor = System.Drawing.Color.White;
+                    }
+                }
+            }
+            else
+            {
+                BackColor = SystemColors.Control;
+                ForeColor = System.Drawing.Color.Black;
+
+                // Светлая тема для всех элементов
+                foreach (Control control in this.Controls)
+                {
+                    if (control is Button || control is TextBox || control is RichTextBox)
+                    {
+                        control.BackColor = SystemColors.Window;
+                        control.ForeColor = System.Drawing.Color.Black;
+                    }
+                }
+            }
+        }
+
+        // Вывод справки
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа для парсинга URL\n" +
+                            "В первой строке можно выбрать файл, в котором содержатся ссылки\n" +
+                            "Во второй строке можно выбрать файл, куда будет помещен результат работы программы\n."+
+                            "В третьей строке можно ввести или вставить из буфера обмена ссылку, которую необходимо разобрать"+
+                            "");
+        }
+    }
+}
